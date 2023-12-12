4f60df40ded54fbcda5ee4ae7cd13fcecf546e0f
Polina <polina9130@gmail.com>
2023-12-12 17:12:39 +0200

    add ui tests to cd pipeline

--- tests/ui/boardgames.test.js
+++ tests/ui/boardgames.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check boardgames page', async ({ page }) => {
-    await page.goto('http://localhost:8080/boardgames');
+    await page.goto('https://boardgames-d7j4.onrender.com/boardgames');
     const list = await page.$('ul');
     expect(list).toBeTruthy();
   });
